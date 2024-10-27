bbe2732c17e60577c6788693d6b76c6836e00c17
Hao2109 <truongtrihao21@gmail.com>
2024-10-27 15:38:12 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Collection from "./pages/Collection";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Order from "./pages/Order";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <Navbar />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/orders" element={<Order />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
